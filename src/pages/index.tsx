e530ca2491757622cd98bdcba5dd749167ada47f
생이(SangE/남상이) <tkddl5990@gmail.com>
2023-03-11 14:13:30 +0900

    feat: 린트 및 프리티어 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-import { Roboto } from "next/font/google";
-import { Head } from "@components/Head";
-
-const inter = Roboto({ weight: ["100", "300", "400", "500", "700", "900"] });
+import { Head } from '@components/Head';
 
 export default function Home() {
   return (
     <>
-      <Head title="재미난거 해야지" />
+      <Head title='재미난거 해야지' />
       <main>
         <p>hello world</p>
       </main>
